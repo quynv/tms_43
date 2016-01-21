735b3f352c8ede1d5fd13324659b3eabcd7b92f9
quynv <quytnhanam@gmail.com>
2016-01-21 15:16:14 +0700

    Activity: view, delete

--- apps/activities/views.py
+++ apps/activities/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
+from django.views import generic
+from apps.activities.models import Activity
+import json
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = "activities/index.html"
+    model = Activity
+    context_object_name = 'activities'
+    paginate_by = 20
+    queryset = Activity.objects.order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
+
+
+class DeleteView(generic.View):
+
+    def post(self, *args, **kwargs):
+        id = self.request.POST.get('id')
+        # report = Report.objects.filter(id=id, user_id=request.user.id);
+        activity = Activity.objects.get(pk=id)
+        if activity:
+            activity.delete()
+            return HttpResponse(
+                json.dumps({
+                    'success': 1,
+                    'message': 'Activity has been removed'
+                }),
+                content_type="application/json"
+            )
+        else:
+            return HttpResponse(
+                json.dumps({
+                    'success': 0,
+                    'message': 'Can\' delete this activity log'
+                }),
+                content_type="application/json"
+            )
--- statics/js/report.js
+++ statics/js/report.js
@@ -27,6 +27,7 @@ $(document).on('click','#new-report', function(){
 $(document).on('click', '.delete-report', function(){
     var id = $(this).data('id');
     var csrf = $(this).data('csrf');
+    var  obj = $(this).parents('.report-content');
     $.ajax({
         url: '/reports/delete/',
         data: {id: id, csrfmiddlewaretoken: csrf},
@@ -35,6 +36,7 @@ $(document).on('click', '.delete-report', function(){
         success: function(data){
             if(data.success == 1){
                Notify( data.message, null, null, "Success");
+                obj.remove();
             } else {
                 Notify( data.message, null, null, "Danger");
             }
