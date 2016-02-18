66b58bf5eb40cd09b16812809162a7e61a82b372
= <=>
2016-02-18 10:55:30 +0700

    edit, change status, delete subject

--- apps/subjects/models.py
+++ apps/subjects/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 from apps.courses.models import Course
 
+status_choise = (
+        ('open', 'Opening'),
+        ('close', 'Closed'),
+)
+
 class Subject(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    status = models.CharField(max_length=10, default='open')
+    status = models.CharField(max_length=10, choices=status_choise, default='open')
     # user_id =
     updated_at = models.DateField(auto_now=True)
     created_at = models.DateField(auto_now_add=True)
