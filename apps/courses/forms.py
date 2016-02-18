b1af4a9d150af0cce066fe9c77de2e65312e157a
= <=>
2016-02-18 14:24:09 +0700

    Task: create, edit, change status, delete

--- apps/courses/forms.py
+++ apps/courses/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Course
 from apps.subjects.models import Subject
+from apps.tasks.models import Task
 
 class CourseForm(forms.ModelForm):
     class Meta:
@@ -33,3 +34,19 @@ class SubjectForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(SubjectForm, self).__init__(*args, **kwargs)
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['name', 'description', 'status']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'description': forms.Textarea(
+                attrs={'rows': 15, 'class': 'form-control', 'style': 'resize:vertical;',
+                    'placeholder': 'Write some thing...'}),
+            'status': forms.Select(
+                            attrs={'class': 'form-control selector', 'title': 'Status'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
